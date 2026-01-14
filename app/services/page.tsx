98418e2b1cad84559bbf0607203eba0cc26eee60
Fahmidul Haque <fahmidulhaque97@pm.me>
2026-01-14 16:05:00 +0000

    add basic services page

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,17 +1,59 @@
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
 import Paper from '@mui/material/Paper';
+import { styled } from '@mui/material/styles';
 
+// const StyledTableCell = styled(TableCell)(({ theme }) => ({
+//   [`&.${tableCellClasses.head}`]: {
+//     backgroundColor: theme.palette.common.black,
+//     color: theme.palette.common.white,
+//   },
+//   [`&.${tableCellClasses.body}`]: {
+//     fontSize: 14,
+//   },
+// }));
+
+// const StyledTableRow = styled(TableRow)(({ theme }) => ({
+//   '&:nth-of-type(odd)': {
+//     backgroundColor: theme.palette.action.hover,
+//   },
+//   // hide last border
+//   '&:last-child td, &:last-child th': {
+//     border: 0,
+//   },
+// }));
 
 export default function Services() {
-    return (
-        <div>
-            <Table></Table>
-        </div>
-    );
+  return (
+    <div>
+      <TableContainer component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Service</TableCell>
+              <TableCell align="right">Price</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            <TableRow>
+              <TableCell>Haircut with Beard Trim</TableCell>
+              <TableCell align="right">£20</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Haircut with Skin Fade</TableCell>
+              <TableCell align="right">£20</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Beard Trim</TableCell>
+              <TableCell align="right">£10</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
+  );
 }
-
